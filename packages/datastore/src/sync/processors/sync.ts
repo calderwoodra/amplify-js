47327e6d0f6791764f4875cfa1c474bce1a02917
Dane Pilcher <dppilche@amazon.com>
2023-03-30 11:45:27 -0600

    feat: add unauthorized hub event

--- packages/datastore/src/sync/processors/sync.ts
+++ packages/datastore/src/sync/processors/sync.ts
@@ -117,6 +117,8 @@ class SyncProcessor {
 
 		// sync only needs the READ auth mode(s)
 		const readAuthModes = modelAuthModes.READ;
+		const authModeErrors = new Array(readAuthModes.length);
+		console.log(readAuthModes);
 
 		let authModeAttempts = 0;
 		const authModeRetry = async () => {
@@ -143,6 +145,7 @@ class SyncProcessor {
 				);
 				return response;
 			} catch (error) {
+				authModeErrors[authModeAttempts] = error;
 				authModeAttempts++;
 				if (authModeAttempts >= readAuthModes.length) {
 					const authMode = readAuthModes[authModeAttempts - 1];
@@ -152,6 +155,15 @@ class SyncProcessor {
 						logger.warn(
 							`User is unauthorized to query ${opName} with auth mode ${authMode}. No data could be returned.`
 						);
+
+						Hub.dispatch('datastore', {
+							event: 'unauthorizedSync',
+							data: {
+								errors: authModeErrors,
+								model: modelDefinition,
+								authModes: readAuthModes,
+							},
+						});
 
 						return {
 							data: {
